e6f8bd3f285ca0ba41baeb9f3ec49a470de81635
James <james@sqrx.com>
2023-12-02 13:19:34 +0800

    Remove unused stuff

--- scripts/build-platform/utils/paths.ts
+++ scripts/build-platform/utils/paths.ts
@@ -3,27 +3,3 @@ import * as path_root from "path";
 export const path = process.cwd().includes(path_root.win32.sep)
   ? path_root.win32
   : path_root.posix;
-
-const MODULE_PATH = "src/modules";
-
-export function maybeAdditionalPath(
-  rootPath: string,
-  additionalPaths: string[]
-) {
-  return additionalPaths.length > 0
-    ? path.join(rootPath, ...additionalPaths)
-    : rootPath;
-}
-
-export function getModulePath(...additionalPaths: string[]) {
-  return maybeAdditionalPath(MODULE_PATH, additionalPaths);
-}
-
-export function isModuleRootDirectory(path: string) {
-  const split = path.split("/");
-  if (split.length !== 3 || split[0] !== "src" || split[1] !== "modules") {
-    return false;
-  }
-
-  return true;
-}
